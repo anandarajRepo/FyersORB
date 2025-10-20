e36edd7199ee486e74b3bc4f4f2820d7d43ecaa1
anandarajRepo <anandaraj76@gmail.com>
2025-10-20 14:55:11 +0530

    Updated python version in batch file

--- run_orb.sh
+++ run_orb.sh
@@ -3,4 +3,4 @@ truncate -s 0 /var/log/orb.log
 truncate -s 0 cd /root/FyersORB/orb.log
 cd /root/FyersORB
 source venv/bin/activate
-python -u main.py run 2>&1 | tee -a orb.log
+python3.11 -u main.py run 2>&1 | tee -a orb.log
